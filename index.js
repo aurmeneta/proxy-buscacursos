1a25a68eeeed0c752f49d9df57c34cf5ed67cd66
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2021-12-17 12:19:24 -0300

    Linting

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require("express")
-require("dotenv").config()
+const express = require('express')
+require('dotenv').config()
 
 const app = express()
-const BUSCACURSOS_URL = "https://buscacursos.uc.cl/"
+const BUSCACURSOS_URL = 'https://buscacursos.uc.cl/'
 
-app.get("/", (req, res) => {
-    res.send("Todo pasando")
+app.get('/', (req, res) => {
+  res.send('Todo pasando')
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Escuchando en ${process.env.PORT || 3000}`)
+  console.log(`Escuchando en ${process.env.PORT || 3000}`)
 })
