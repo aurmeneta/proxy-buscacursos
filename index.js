5ff3e9438ec2e6f45e33e5293450c51f89f495bc
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2021-12-17 12:18:47 -0300

    Dummy server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Todo pasando')
+const express = require("express")
+require("dotenv").config()
+
+const app = express()
+const BUSCACURSOS_URL = "https://buscacursos.uc.cl/"
+
+app.get("/", (req, res) => {
+    res.send("Todo pasando")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Escuchando en ${process.env.PORT || 3000}`)
+})
