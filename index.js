a535a96bb3db65bfed195662e712012104532dc5
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2021-12-18 23:27:56 -0300

    Functional Proxy

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 require('dotenv').config()
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const cors = require('cors')
 
 const app = express()
 const BUSCACURSOS_URL = 'https://buscacursos.uc.cl/'
 
-app.get('/', (req, res) => {
-  res.send('Todo pasando')
+// Middleware para logging
+app.use((req, _res, next) => {
+  console.log('Request: ' + req.originalUrl)
+  next()
 })
 
+// CORS
+app.use(cors())
+
+// Proxy
+app.use(createProxyMiddleware({ target: BUSCACURSOS_URL, changeOrigin: true }))
+
+// Iniciar app
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Escuchando en ${process.env.PORT || 3000}`)
 })
